d18d2291cb9c407a451115d9d31ecf770e2fad0b
Stock Archery <stockarchery.bihar@gmail.com>
2025-01-28 15:47:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -265,8 +265,8 @@ def fetch_stock(symbol):
             return symbol, None
 
         # Get the most recent and the previous day's closing prices
-        previous_close = closing_prices.iloc[-1]
-        current_price = closing_prices.iloc[0]
+        previous_close = closing_prices.iloc[-2]
+        current_price = closing_prices.iloc[-1]
 
         # Calculate the percentage change
         percentage_change = ((current_price - previous_close) / previous_close) * 100
